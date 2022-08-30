b270ab20d6d63d20e6bb1cd25a8362843a73df44
Alejandro Acero <alejandroacero2012@gmail.com>
2022-08-30 14:56:36 -0500

    moreChanges

--- client/script.js
+++ client/script.js
@@ -221,3 +221,12 @@ function onMouseLeave() {
 
 document.querySelector(".social").target = "blank"
 document.querySelector("#linked").target = "blank"
+let body = document.querySelector("body")
+
+// if( (document.documentElement.scrollHeight) <500) || (document.documentElement.scrollWidth) <700 || (document.documentElement.scrollWidth<document.documentElement.scrollHeight){
+//   console.log('e')
+// }
+
+if( (document.documentElement.scrollHeight) <500 || (document.documentElement.scrollWidth) <700 ) {
+  body.innerHTML = "<p>Sorry! The mobile version of this website is not yet ready. Come back later!</p>"
+}
