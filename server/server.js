2de97c090eef70152b4a7c7ce616957eb44d0c45
Alejandro Acero <alejandroacero2012@gmail.com>
2022-08-30 15:07:23 -0500

    oops

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ app.post('/api/pets', getPicture)
 
 
 
-
-app.listen(4000,console.log("Server running on 4000"))
+const port = process.env.PORT || 4000
+app.listen(port,console.log("Server running on 4000"))
